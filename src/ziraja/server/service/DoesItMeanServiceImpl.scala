0a88a54aecb4f165a2e3152526a192ed62b6d5f1
Tok <lsteiger4@gmail.com>
2013-01-28 00:04:47 +0100

    DoesItMeanService improved.

--- src/ziraja/server/service/DoesItMeanServiceImpl.scala
+++ src/ziraja/server/service/DoesItMeanServiceImpl.scala
@@ -7,7 +7,9 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet
 import ziraja.client.service.DoesItMeanService
 
 class DoesItMeanServiceImpl extends RemoteServiceServlet with DoesItMeanService {
-  val indent = "Did you mean:"
+  val dym = "Did you mean:"
+  val sif = ">Search instead for</span>"
+  val srf = ">Showing results for</span>"
 
   /**
    * Sends a query to google and returns the value of the
@@ -16,7 +18,10 @@ class DoesItMeanServiceImpl extends RemoteServiceServlet with DoesItMeanService 
   override def doesItMean(input: String): String = {
     try {
       val src = io.Source.fromURL("http://www.google.com/search?lang=en&hl=en&q=" + input).mkString
-      if (src != null && src.contains(indent)) ((src.split(indent)(1)).split("<i>")(1)).split("</i>")(0) else "???"
+      if (src != null && src.contains(dym)) ((src.split(dym)(1)).split("<i>")(1)).split("</i>")(0)
+      else if (src != null && src.contains(sif)) ((src.split(sif)(1)).split(">")(1)).split("</a>")(0)
+      else if (src != null && src.contains(srf)) ((src.split(srf)(1)).split("<i>")(1)).split("</i>")(0)
+      else "???"
     } catch {
       case murle: MalformedURLException => "FAIL: URL malformed."
       case ioe: IOException => "??"
